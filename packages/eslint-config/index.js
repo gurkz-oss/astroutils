5f0ba26adc28261d40c7f806237c740fa4c193d3
Gurkan <hello@gurkz.me>
2024-03-05 16:02:26 +0100

    setup monorepo with eslint

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,17 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+};
