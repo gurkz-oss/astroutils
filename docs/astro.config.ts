6256b41c127e7da88d28b9027f711e7459d24309
Gurkan <hello@gurkz.me>
2024-03-06 20:07:24 +0100

    auto deploy to gh pages

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -3,6 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://docs.gurkz.me",
   integrations: [
     starlight({
       title: "astroutils documentation",
